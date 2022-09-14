e124d8ac1e829c0d89f375bc745c0e64f0931bc1
aufaarik <rizkiaufa123@gmail.com>
2022-09-14 13:08:58 +0700

    commit

---
+++ Cabang.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Cabang {
+
+    public static void main (String[] args) {
+        Scanner inp = new Scanner (System.in);
+
+        String nama;
+        int nilai;
+
+        System.out.println("Selamat Datang");
+        System.out.print("Masukkan Nama : ");
+        nama = inp.nextLine();
+        System.out.print("Masukkan Nilai : ");
+        nilai = inp.nextInt();
+
+        if (nilai >= 60){
+            System.out.println("Selamat "+nama+",Anda Lulus");
+        }else{
+            System.out.println("Mohon Maaf"+ nama +", Anda Tidak Lulus");
+        }
+    }
+    
+}
